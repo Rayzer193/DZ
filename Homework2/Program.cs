a47ac375d73ef309f0c32b8e3c83084f053ee4e5
Ivan Pavlov <rayzer_s@mail.ru>
2022-10-14 13:39:27 +0300

    3 task

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.Write("Введите первое число:");
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+Console.Write("Введите первое число:");
 int number1 = int.Parse(Console.ReadLine());
 Console.Write("Введите второе число:");
 int number2 = int.Parse(Console.ReadLine());
